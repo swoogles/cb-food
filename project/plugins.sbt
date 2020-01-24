b8fd7ba9dd5c4efa84edfa910b6575b6e05f3292
bfrasure <bill.frasure@gmail.com>
2020-01-23 20:32:44 -0700

    Tinker with Schedules representing buses. Also, unsuccessful attempts at using Zio's Clock (Which relies on java.time) in ScalaJS

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,4 @@
 addSbtPlugin("org.jmotor.sbt" % "sbt-dependency-updates" % "1.2.1")
+//addSbtPlugin("org.scala-js" % "sbt-scalajs" % "0.6.29")
+addSbtPlugin("org.scala-js" % "sbt-scalajs" % "0.6.31")
+//addSbtPlugin("org.scala-js" % "sbt-scalajs" % "1.0.0-RC2")
--- src/main/scala/MyApp.scala
+++ src/main/scala/MyApp.scala
@@ -1,17 +1,47 @@
-import zio.{App, ZIO}
+import java.io.IOException
+import java.time.LocalTime
+
+import zio.{App, Schedule, ZIO}
 import zio.console._
 import zio.clock._
+import zio._
+import zio.duration.Duration
 
 object MyApp extends App {
+  def numberOfBussesPerDay(spacing: Duration, start: LocalTime, end: LocalTime) = ???
+
+  val driveFromMountaineerToTeocalli =
+    Schedule.once && Schedule.spaced(duration.durationInt(2).seconds)
+
+  def realBusSchedule(numberOfBussesPerDay: Int): Schedule[Clock, Any, (Int, Int)] =
+    Schedule.recurs(numberOfBussesPerDay) &&
+      Schedule.spaced(duration.durationInt(2).seconds)
+
+  val liveBusses: ZIO[Clock with Console, Nothing, (Int, Int)] =
+
+    putStrLn("Bus is leaving Mountaineer Square!")
+      .flatMap(_ => ZIO.sleep(duration.durationInt(2).seconds))
+      .flatMap(_ => putStrLn("Bus arrived at teocalli!"))
+      .repeat(realBusSchedule(5))
+
   // TODO ScalaJS version of App
   override def run(args: List[String]): ZIO[zio.ZEnv, Nothing, Int] = {
-    val logic = for {
+    val repetitions = Schedule.recurs(3) && Schedule.spaced(duration.durationInt(1).second)
+    val logic: ZIO[Console with Clock, IOException, Int] = for {
       _ <- putStr("name: ")
-      name <- getStrLn
+      name <- ZIO.succeed("Hardcoded Harry")
       now <- currentDateTime
       _ <- putStrLn(s"hello, $name - $now")
-    } yield ()
+    } yield (0)
+    logic
+      .repeat(repetitions)
 
-    logic.fold(_ => 1, _ => 0)
+    .fold(
+      error => 1,
+      success => 0)
+
+    liveBusses
+      .flatMap{case (left, right)=> putStrLn("left: " + left + " right: " + right)}
+      .map( _ => 0)
   }
 }
