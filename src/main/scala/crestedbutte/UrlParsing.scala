5bc65f7329e44c9a3e46efcd27fc0ea2f27a0aaa
bfrasure <bill.frasure@gmail.com>
2020-02-16 01:16:58 -0700

    Fix error when *no* queryParams are present

--- src/main/scala/crestedbutte/UrlParsing.scala
+++ src/main/scala/crestedbutte/UrlParsing.scala
@@ -10,17 +10,19 @@ object UrlParsing {
       .getPath
 
   def getUrlParameters(url: String): Map[String, Array[String]] =
-    java.net.URI
-      .create(url)
-      .getQuery
-      .split('&')
-      .map(js.URIUtils.decodeURIComponent)
-      .map { p =>
-        val split = p.split('=')
-        (split.head, split.tail.mkString("="))
-      }
-      .groupBy(_._1)
-      .map(m => m._1 -> m._2.map(_._2))
+    if (url.contains('?'))
+      java.net.URI
+        .create(url)
+        .getQuery
+        .split('&')
+        .map(js.URIUtils.decodeURIComponent)
+        .map { p =>
+          val split = p.split('=')
+          (split.head, split.tail.mkString("="))
+        }
+        .groupBy(_._1)
+        .map(m => m._1 -> m._2.map(_._2))
+    else Map()
 
   def getUrlParameter(url: String,
                       parameter: String): Option[String] =
