15e5c8c16985139d0c4171321175d024de66359b
bfrasure <bill.frasure@gmail.com>
2020-02-18 20:02:59 -0700

    Convert all tests to use ScheduleAtStop class

--- src/test/scala/BusTimeCalculationsSpec.scala
+++ src/test/scala/BusTimeCalculationsSpec.scala
@@ -31,35 +31,33 @@ object BusTimeCalculationsSpec
           "find the next bus time while in the middle of the scehdule"
         ) {
           assert(
-            BusTimeCalculations.nextBusArrivalTime(
-              List(BusTime("09:00"), BusTime("09:15")), // TODO This callsite (and whole test class) screams for a proper BusSchedule type
-              BusTime("09:10")
-            ),
+              ScheduleAtStop(
+                List(BusTime("09:00"), BusTime("09:15")),
+              ).nextBusArrivalTime(BusTime("09:10")
+              ),
             isSome(equalTo(BusTime("09:15")))
           )
         },
         test("really early morning check") {
           assert(
-            BusTimeCalculations.nextBusArrivalTime(
-              List(BusTime("07:10")), // TODO This callsite (and whole test class) screams for a proper BusSchedule type
-              BusTime("05:00")
-            ),
+              ScheduleAtStop(
+                List(BusTime("07:10")),
+              ).nextBusArrivalTime(BusTime("05:00")),
             isSome(equalTo(BusTime("07:10")))
           )
         },
         test("after last bus has run") {
-          assert(BusTimeCalculations.nextBusArrivalTime(
-                   List(BusTime("23:40")),
-                   BusTime("23:50")
-                 ),
+          assert(
+              ScheduleAtStop(
+                List(BusTime("23:40")),
+              ).nextBusArrivalTime(BusTime("23:50")),
                  equalTo(Option.empty))
         },
         test("bus is arriving this minute") {
           assert(
-            BusTimeCalculations.nextBusArrivalTime(
-              List(BusTime("23:40")),
-              BusTime("23:40")
-            ),
+              ScheduleAtStop(
+                List(BusTime("23:40")),
+              ).nextBusArrivalTime(BusTime("23:40")),
             isSome(equalTo(BusTime("23:40")))
           )
         }
