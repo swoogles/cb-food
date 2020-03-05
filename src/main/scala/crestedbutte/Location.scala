b0006ba0b4d5743878e362b31e6595b0f772d029
bfrasure <bill.frasure@gmail.com>
2020-03-04 19:52:27 -0700

    #time 15m Shrink required info for Locations

--- src/main/scala/crestedbutte/Location.scala
+++ src/main/scala/crestedbutte/Location.scala
@@ -3,10 +3,14 @@ package crestedbutte
 object Location extends Enumeration {
 
   protected case class Val(name: String,
-                           gpsCoordinates: GpsCoordinates,
-                           elementName: String,
-                           altName: String)
-      extends super.Val(name)
+                           altName: String = "",
+                           gpsCoordinates: GpsCoordinates =
+                             GpsCoordinates(0, 0))
+      extends super.Val(name) {
+
+    val elementName =
+      name.map(letter => if (letter.isLetter) letter else "_")
+  }
   import scala.language.implicitConversions
 
   implicit def valueToStopLocationVal(x: Value): Val =
@@ -17,168 +21,115 @@ object Location extends Enumeration {
   // TODO Check ordering of all coordinates
   val OldTownHall: Val =
     Val("Old Town Hall",
-        GpsCoordinates(38.869538, -106.987547),
-        "old_town_hall",
-        "(Malardi Theater)")
+        "(Malardi Theater)",
+        GpsCoordinates(38.869538, -106.987547))
 
   val Clarks: Val = Val("6th/Belleview",
-                        GpsCoordinates(38.866970, -106.981499),
-                        "clarks",
-                        "(Clarks Grocery)") //
+                        "(Clarks Grocery)",
+                        GpsCoordinates(38.866970, -106.981499)) //
 
-  val FourWayUphill: Val = Val("4-way",
-                               GpsCoordinates(38.870355, -106.980905),
-                               "fourway_uphill",
-                               "(To Mountain)") // gps
+  val FourWayUphill: Val = Val(
+    "4-way",
+    "(To Mountain)",
+    GpsCoordinates(38.870355, -106.980905)
+  ) // gps
   val TeocalliUphill: Val = Val(
     "Teocalli",
-    GpsCoordinates(38.872718, -106.980830),
-    "teocalli_uphill",
-    "(To Mountain)"
+    "(To Mountain)",
+    GpsCoordinates(38.872718, -106.980830)
   ) //
   val MountaineerSquare: Val = Val(
     "Mountaineer Square",
-    GpsCoordinates(38.900902, -106.966650),
-    "mountaineer_square",
-    "(CBMR)"
+    "(CBMR)",
+    GpsCoordinates(38.900902, -106.966650)
   ) //  // This is rough. Maps seems to be off...
   val TeocalliDownhill: Val = Val(
     "Teocalli",
-    GpsCoordinates(38.872726, -106.981037),
-    "teocalli_downhill",
-    "(To Downtown)"
+    "(To Downtown)",
+    GpsCoordinates(38.872726, -106.981037)
   ) //
   val FourwayDownhill: Val = Val(
     "4-way",
-    GpsCoordinates(38.869944, -106.981503),
-    "fourway_downhill",
-    "(To Downtown)"
+    "(To Downtown)",
+    GpsCoordinates(38.869944, -106.981503)
   ) //
 
   // Condo loop entries
   val ThreeSeasons: Val =
-    Val("Three Seasons", GpsCoordinates(0, 0), "three_seasons", "")
+    Val("Three Seasons")
 
   val MountainSunrise: Val =
-    Val("Mountain Sunrise",
-        GpsCoordinates(0, 0),
-        "mountain_sunrise",
-        "")
+    Val("Mountain Sunrise")
 
   val UpperChateaux: Val =
-    Val("Upper Chateaux", GpsCoordinates(0, 0), "upper_chateaux", "")
+    Val("Upper Chateaux")
 
-  val LowerChateaux: Val = Val("Lower Chateaux / Marcellina",
-                               GpsCoordinates(0, 0),
-                               "lower_chateaux",
-                               "")
+  val LowerChateaux: Val = Val("Lower Chateaux / Marcellina")
 
   // RTA stops.
   val GunnisonCommunitySchools: Val = Val(
-    "Gunnison Community Schools",
-    GpsCoordinates(0, 0),
-    "gunnison_community_schools",
-    ""
+    "Gunnison Community Schools"
   )
 
-  val EleventhAndVirginia: Val = Val("Eleventh & Virgina",
-                                     GpsCoordinates(0, 0),
-                                     "eleventh_and_virginia",
-                                     "")
+  val EleventhAndVirginia: Val = Val("Eleventh & Virgina")
 
   val Safeway: Val =
-    Val("Safeway",
-        GpsCoordinates(0, 0),
-        "Safeway",
-        "(Spruce & Highway 50)")
+    Val("Safeway", "(Spruce & Highway 50)")
 
   val TellerAndHighwayFifty: Val =
-    Val("Teller & Highway 50",
-        GpsCoordinates(0, 0),
-        "teller_and_highway_fifty",
-        "")
+    Val("Teller & Highway 50", "")
 
   val Western: Val =
-    Val("Western", GpsCoordinates(0, 0), "Western", "Colorado & Ohio")
+    Val("Western", "Colorado & Ohio")
 
   val DenverAndHighwayOneThirtyFive: Val =
-    Val("Denver & Highway 135",
-        GpsCoordinates(0, 0),
-        "denver_and_highway_one_thirty_five",
-        "(City Market)")
+    Val("Denver & Highway 135", "(City Market)")
 
   val SpencerAndHighwayOneThirtyFive: Val =
-    Val("Spencer & Highway 135",
-        GpsCoordinates(0, 0),
-        "spencer_and_highway_one_thirty_five",
-        "(Walmart)")
+    Val("Spencer & Highway 135", "(Walmart)")
 
   val TallTexan: Val =
-    Val("TallTexan",
-        GpsCoordinates(0, 0),
-        "tall_texan",
-        "(Flag Stop)")
+    Val("TallTexan", "(Flag Stop)")
 
   val OhioCreek: Val =
-    Val("OhioCreek",
-        GpsCoordinates(0, 0),
-        "ohio_creek",
-        "(Flag Stop)")
+    Val("OhioCreek", "(Flag Stop)")
 
   val Almont: Val =
-    Val("Almont", GpsCoordinates(0, 0), "almont", "(Flag Stop)")
+    Val("Almont", "(Flag Stop)")
 
   val CBSouth: Val =
-    Val("CB South",
-        GpsCoordinates(0, 0),
-        "CB_South",
-        "(Red Mtn Park)")
+    Val("CB South", "(Red Mtn Park)")
 
   val Riverland: Val =
-    Val("Riverland", GpsCoordinates(0, 0), "Riverland", "(Flag Stop)")
+    Val("Riverland", "(Flag Stop)")
 
   val BrushCreek: Val =
-    Val("Brush Creek",
-        GpsCoordinates(0, 0),
-        "brush_creek",
-        "(Flag Stop)")
+    Val("Brush Creek", "(Flag Stop)")
 
   val Riverbend: Val =
-    Val("Riverbend", GpsCoordinates(0, 0), "Riverbend", "(Flag Stop)")
+    Val("Riverbend", "(Flag Stop)")
 
   // BEGIN Condo loop stops
   val Whetstone: Val =
-    Val("Whetstone", GpsCoordinates(0, 0), "Whetstone", "")
+    Val("Whetstone")
 
   val ColumbineCondo: Val =
-    Val("ColumbineCondo", GpsCoordinates(0, 0), "ColumbineCondo", "")
+    Val("ColumbineCondo")
 
   val CinnamonMtn: Val =
-    Val("Cinnamon Mtn / Gothic",
-        GpsCoordinates(0, 0),
-        "Cinnamon_Mtn_Gothic",
-        "")
+    Val("Cinnamon Mtn / Gothic")
 
   val MtCbTownHall: Val =
-    Val("Mt CB Town Hall", GpsCoordinates(0, 0), "MtCbTownHall", "")
+    Val("Mt CB Town Hall")
 
   val UpperParadiseRoad: Val =
-    Val("UpperParadiseRoad",
-        GpsCoordinates(0, 0),
-        "UpperParadiseRoad",
-        "")
+    Val("UpperParadiseRoad")
 
   val LowerParadiseRoad: Val =
-    Val("LowerParadiseRoad",
-        GpsCoordinates(0, 0),
-        "LowerParadiseRoad",
-        "")
+    Val("LowerParadiseRoad")
 
   val EaglesNestCondos: Val =
-    Val("EaglesNestCondos",
-        GpsCoordinates(0, 0),
-        "EaglesNestCondos",
-        "")
+    Val("EaglesNestCondos")
   // END Condo loop stops
 
 }
