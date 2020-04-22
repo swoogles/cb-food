f83e34b676cd189fb586f5c8bfcd340cef6b3e64
bfrasure <bill.frasure@gmail.com>
2020-04-21 20:08:59 -0600

    #time 20m Break ground on GunnisonRestaurants w/ 5 B's BBQ

---
+++ src/main/scala/crestedbutte/routes/GunnisonRestaurants.scala
@@ -0,0 +1,37 @@
+package crestedbutte.routes
+
+import crestedbutte.time.{ClosedAllDay, Hours, HoursOfOperation}
+import crestedbutte.{
+  Location,
+  PhoneNumber,
+  RestaurantGroupName,
+  RestaurantWithSchedule,
+  StandardSchedule,
+  Website,
+}
+
+object GunnisonRestaurants
+    extends RestaurantGroup(
+      RestaurantGroupName("Crested Butte Restaurants"),
+      Seq(
+        RestaurantWithSchedule(
+          Location("5 B's Bar-B-Q"),
+          PhoneNumber("970-641-7360", "Order!"),
+          Website.global("https://www.5bsbbq.com/"),
+          Website.facebookPage(
+            "https://www.facebook.com/5BsBBQ/",
+          ),
+          StandardSchedule.carryOutAndDelivery(
+            HoursOfOperation(
+              sunday = ClosedAllDay,
+              monday = ClosedAllDay,
+              tuesday = Hours("11:00", "19:00"),
+              wednesday = Hours("11:00", "19:00"),
+              thursday = Hours("11:00", "19:00"),
+              friday = Hours("11:00", "19:00"),
+              saturday = Hours("11:00", "19:00"),
+            ),
+          ),
+        ),
+      ),
+    )
